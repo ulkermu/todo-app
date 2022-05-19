efd5da9bbac84f1dc505e85fa483923b735d260c
Murat Ülker <muratulker93@gmail.com>
2022-05-19 05:07:29 +0300

    Deleted React Setup Files v2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import Todo from "./Todo";
 
 function App() {
   return (
     <div className="App">
+      <Todo />
     </div>
   );
 }
---
+++ src/components/Todo.jsx
@@ -0,0 +1,10 @@
+const Todo = () => {
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <span>Bu bir todolist alanıdır.</span>
+    </div>
+  );
+};
+
+export default Todo;
