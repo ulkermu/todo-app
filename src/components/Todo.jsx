5c7d8c1d0b6a799a7c88a80fe3f8128a44d972ec
Murat Ülker <muratulker93@gmail.com>
2022-05-21 21:20:12 +0300

    Todo v0.01

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -2,11 +2,14 @@ import { useContext } from "react";
 import TodoContext from "../context/TodoContext";
 
 const Todo = () => {
-  const {content} = useContext(TodoContext);
+  const {content, dispatch} = useContext(TodoContext);
   return (
     <div className="Todo">
       {content.map((content, id) => (
-        <div className="TodoChildren" key={id}>{content.todoContent}</div>
+        <div className="TodoChildren" key={id}>
+          {content.todoContent}
+          <button onClick={() => dispatch({type: 'remove_content', id: content.id})} className="TodoDelete">X</button>
+        </div>
       ))}
     </div>
   );
